e7f96678b57d6c7d1247e9da1a3302a8c1656673
Fat Dog <2149436837@qq.com>
2026-07-09 21:50:46 +0800

    02

---
+++ Day1/02/Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+    public static void main(String[] args) {
+        //变量的声明(包含数据类型，变量名称)
+        String name;
+        //变量的赋值
+        name = "fatdog";
+
+        /*
+        合并两部
+        String name = "fatdog"
+        */
+        //变量的使用
+        System.out.println(name);
+    }
+}
