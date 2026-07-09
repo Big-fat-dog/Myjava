63faa8e49ff7dc41c281b77261b8a226138b3bb6
Fat Dog <2149436837@qq.com>
2026-07-09 21:51:37 +0800

    02

---
+++ Day1/01/hello1.java
@@ -0,0 +1,9 @@
+public class hello1 {
+    public static void main(String[] args){
+        /*
+        内存中具有数据存储和向量存储
+        也就是变量的底层，这让cpu通过变量执行不同的操作
+        */
+        System.out.println("Hello,world");
+    }
+}
