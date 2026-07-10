7f7ffb5ec4d46c9e4a0c029fe224daf4c5dad6c4
Fat Dog <2149436837@qq.com>
2026-07-10 09:46:57 +0800

    1

---
+++ Day2/DataType.java
@@ -0,0 +1,30 @@
+package Day2;
+public class DataType {
+    public static void main(String[] args) {
+        //1.比特(bit位)：数据运算的最小存储单位
+        //2.字节(byte)：数据最小存储单位。  1byte = 8bit
+        //1024byte = 1kb
+
+        //TODO1.整数类型
+        //byte:8位
+        byte b = 10;
+        //short：16位
+        short s =10;
+        //int：32位
+        int i = 10;
+        //long：64位
+        long l = 10;
+
+        //TODO2.浮点类型：含有小数点(默认的小数是双精度类型，由于数据需要匹配，所以float得在后面加上f)
+        //float:单精度
+        float f = 1.0f;
+        //double:双精度
+        double d = 1.0;
+
+        //TODO3.字符类型
+        char c = '0';
+
+        //TODO4.布尔类型
+        boolean bl = true;
+    }
+}
